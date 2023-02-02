cc2c6e38e6b00541c1dbd0c933c8a503485cb205
Mark Strempel <mark.strempel@codecool.com>
2023-02-02 10:59:24 +0100

    Species model improvements.
    Made all fields required and added conditional requirement for the parent field.

--- server/db/species.model.js
+++ server/db/species.model.js
@@ -4,11 +4,28 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const SpeciesSchema = new Schema({
-	name: String,
-	extinct: Boolean,
-	_id: Number,
-	parent: Number,
-	confidence: String,
+	name: {
+		type: String,
+		required: true 
+	},
+	extinct: {
+		type: Boolean,
+		required: true
+	},
+	importId: {
+		type: Number,
+		required: true
+	},
+	parent: {
+		type: mongoose.Schema.ObjectId,
+		required: [function() {
+			return this.importId != 1;
+		}, "The parent field is required for all nodes except the 'Life on Earth' node!"]
+	},
+	confidence: {
+		type: String,
+		required: true
+	},
 });
 
 module.exports = mongoose.model("Species", SpeciesSchema);
