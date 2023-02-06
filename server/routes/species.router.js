e4624af5c9f4d262bf0d7d9a4aeb0cfe4b07ed22
Mark Strempel <mark.strempel@codecool.com>
2023-02-06 14:30:36 +0100

    Added end points for phylogentic tree viewer.

--- server/routes/species.router.js
+++ server/routes/species.router.js
@@ -10,4 +10,21 @@ speciesRouter.get("/", async (req, res) => {
 	return res.json(species);
 });
 
+speciesRouter.get("/:speciesId", async (req, res) => {
+	const speciesId = req.params.speciesId
+	const species = await SpeciesModel
+		.findOne({importId: speciesId})
+	return res.json(species);
+});
+
+speciesRouter.get("/:speciesId/descendants", async (req, res) => {
+	const speciesId = req.params.speciesId
+	const species = await SpeciesModel
+		.findOne({importId: speciesId})
+
+	const descendants = await SpeciesModel
+		.find({parent: species._id});
+	return res.json(descendants);
+});
+
 module.exports = speciesRouter;
