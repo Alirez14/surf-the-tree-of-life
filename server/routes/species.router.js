57d47ba1ec9187954c706329986d76f8637a960c
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-02-09 16:46:48 +0100

    server: Remove `return` from /api/species endpoint

--- server/routes/species.router.js
+++ server/routes/species.router.js
@@ -7,7 +7,7 @@ speciesRouter.get("/", async (req, res) => {
 	const species = await SpeciesModel.find()
 		.limit(100)
 		.sort({ created: "asc" });
-	return res.json(species);
+	res.json(species);
 });
 
 module.exports = speciesRouter;
